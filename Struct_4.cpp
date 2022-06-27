50fc3d687e63a4f79e97e6ac76a15510c01f08dc
aondenet-sinval <sinvalsatv@gmail.com>
2022-06-27 15:26:22 -0300

    Estruturas aninhadas com arrays finalização da correção de erros

--- Struct_4.cpp
+++ Struct_4.cpp
@@ -10,17 +10,29 @@ Pedir os dados ao usuário:
 2 - detalhes a ser cadastrado.
 Apresentar na tela os carros registrados que foram inseridos in memory.
 Problema apresentado na execução desse programa:
-Aguns dados tem sido saltados durante a execução
+1 - Aguns eram saltados durante a execução.
 Exemplo:
 Após digitar a QTDE e apertar enter  acontece um pulo e o próximo dado solicitado é a marca e não a cor.
+Conforme alguns programadores comentam no stackoverflow usei cin.ignore comentado abaixo.
+O salto se deve a quebra de linha \n.
+Mas o programa não prosseguia dependendo do uso (ao usar cin.ignore() passando alguns parametros).
+Mantive por isso somente em algumas partes do programa.
+Mas alterei a estrutura evitando misturar cin com cin.getline().
+Achei que o resultado foi satisfatório
+2 - Estava declarando na função cadastrarCarro a seguinte instrução:
+oCarro[indice].DetalhesPlaca.placa[6]
+oCarro[indice].DetalhesAno.ano[4]
+Isso fazia os valores retornarem em main com o endereço alocado na memória e não com o valor da variável.
+Embora o valor da variável estivesse na função cadastrarCarro.
+Então refiz esse trecho do código.
 */
 #define LENCHAR 15
 
 struct Carro{
     char cor[LENCHAR];
     char marca[LENCHAR];
-    int placa[6];
-    int ano[4];
+    int placa;
+    int ano;
 }Carro;
 
 struct CarroEletrico{
@@ -31,33 +43,36 @@ struct CarroEletrico{
     struct Carro DetalhesAno;
 }CarroEletrico;
 
-struct CarroEletrico ModeloFuturo;
 
 void cadastrarCarro(struct CarroEletrico oCarro[], int indice){
+    cout << "Insira o tempo de carga da bateria: " << endl;
+    cin.getline(oCarro[indice].TempoCarga, LENCHAR);
     cout << "Insira a cor do carro" << endl;
     cin.getline(oCarro[indice].DetalhesCor.cor, LENCHAR);
     cout << "Insira a marca do carro" << endl;
     cin.getline(oCarro[indice].DetalhesMarca.marca, LENCHAR);
     cout << "Insira o número da placa" << endl;
-    cin >> oCarro[indice].DetalhesPlaca.placa[6];
-    while(oCarro[indice].DetalhesPlaca.placa[6] == false){//teste para ver se foi deixado em branco.
-        cin >> oCarro[indice].DetalhesPlaca.placa[6];
-    };
+    cin >> oCarro[indice].DetalhesPlaca.placa;
+//    cin.ignore('\n');
+//    while(oCarro[indice].DetalhesPlaca.placa[6] == false){//teste para ver se foi deixado em branco.
+//        cin >> oCarro[indice].DetalhesPlaca.placa[6];
+//    };
     cout << "Insira o ano de fabricação" << endl;
-    cin >> oCarro[indice].DetalhesAno.ano[4];
-    cout << "Insira o tempo de carga da bateria: " << endl;
-    cin.getline(oCarro[indice].TempoCarga, LENCHAR);
+    cin >> oCarro[indice].DetalhesAno.ano;
+    cin.ignore();
 }
 
 void imprimirCarro(struct CarroEletrico novoCarro){
-cout << "Cor " << novoCarro.DetalhesCor.cor << " marca " << novoCarro.DetalhesMarca.marca << " placa " << novoCarro.DetalhesPlaca.placa << " ano " << novoCarro.DetalhesAno.ano << " e tempo de carga da bateria" << novoCarro.TempoCarga << endl;
+cout << "Cor " << novoCarro.DetalhesCor.cor << " marca " << novoCarro.DetalhesMarca.marca << " placa " << novoCarro.DetalhesPlaca.placa << " ano " << novoCarro.DetalhesAno.ano << " e tempo de carga da bateria " << novoCarro.TempoCarga << "."<< endl;
 }
 
 
 int main(){
 int QTDE;
-cout << "Quantos carros você deseja cadastrar agora? " << endl;
+//Deixando o usuário do programa escolher a quanidade de carros a ser cadastrada.
+cout << "Digite a quantidade de carros para cadastro: " << endl;
 cin >> QTDE;
+cin.ignore();
 struct CarroEletrico novosCarros[QTDE];//inicialização
 int i;
     //Registro de novos carros inicando com o índice 0
