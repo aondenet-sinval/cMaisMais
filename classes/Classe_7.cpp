549bde213dc64cc123d91b3929c3f94873bfc18f
aondenet-sinval <sinvalsatv@gmail.com>
2022-07-02 17:25:52 -0300

    polimorfismo

---
+++ classes/Classe_7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//Aplicando conceito de herança com polimorfismo ou multiplas formas
+class Passaro{
+    public:
+    void Som(){
+        cout << "Canto do passaro..." << endl;
+    }
+};
+
+class BemTeVi: public Passaro{
+    public:
+    void Som(){
+        cout << "Te te vi..." << endl;
+    }
+};
+
+class Coruja: public BemTeVi{
+    public:
+    void Som(){
+        cout << "Grash grash..." << endl;
+    }
+};
+
+int main(){
+    Passaro MeuPassaro;
+    BemTeVi TeVi;
+    Coruja Croco;
+
+    MeuPassaro.Som();//método
+    TeVi.Som();//método
+    Croco.Som();//método
+    return 0;//o mesmo método aplicado de forma diferente
+}
